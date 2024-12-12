caa808fbbe2fe13cfe26fd5af8bd874be9e4ac93
Nathan Baranow <e12227714@student.tuwien.ac.at>
2024-12-12 16:32:55 +0100

    #46 fix locationTest bug

--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/unittests/service/CustomLocationServiceTest.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/unittests/service/CustomLocationServiceTest.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.sepr.groupphase.backend.unittests.service;
 
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.*;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.LocationMapper;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Location;
 import at.ac.tuwien.sepr.groupphase.backend.exception.ConflictException;
 import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
@@ -28,10 +29,13 @@ public class CustomLocationServiceTest {
     @Mock
     private LocationValidator locationValidator;
 
+    @Mock
+    private LocationMapper locationMapper;
+
     @BeforeEach
     void setup() {
         MockitoAnnotations.openMocks(this);
-        locationService = new CustomLocationService(locationRepository, locationValidator);
+        locationService = new CustomLocationService(locationRepository, locationValidator, locationMapper);
     }
 
     @Test
