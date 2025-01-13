a56dbb7ab16211c79a9dcafa6b7d570f7f011024
Aida <smajlovicaida567@gmail.com>
2025-01-13 02:40:27 +0100

    #76 fix checkstyle

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ReservedEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/ReservedEndpoint.java
@@ -12,7 +12,15 @@ import jakarta.annotation.security.PermitAll;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 
 import java.lang.invoke.MethodHandles;
 import java.util.List;
